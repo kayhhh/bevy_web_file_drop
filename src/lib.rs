f1ebd6e6b9a9b2174f396eabfdfabffe12488a20
Kayh <basedlainpilled@gmail.com>
2024-01-03 00:42:33 -0500

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@
 //!
 //! ## Usage
 //!
-//! After adding the plugin, you can use the same `FileDragAndDrop` events to read dropped files.
+//! After adding the plugin, you can use the same Bevy `FileDragAndDrop` events to read dropped files.
 //! The only difference is that the `path_buf` field of the `DroppedFile` event will be a blob URL instead of a local file path.
-//! This blob can be loaded as a Bevy asset using [bevy_blob_loader](https://docs.rs/bevy_blob_loader/latest/bevy_blob_loader/).
+//! This blob URL will be loadable as a Bevy asset using [bevy_blob_loader](https://docs.rs/bevy_blob_loader/latest/bevy_blob_loader/).
+//!
+//! You might not have to change any code at all! Just add in the plugin and dropped files should work in both native and web builds.
 
 use std::path::Path;
 
