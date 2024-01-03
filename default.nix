ccfe666ee36d3c949e84e2f8fdee52ecd74e6174
Kayh <basedlainpilled@gmail.com>
2024-01-02 21:54:39 -0500

    init repo

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, system, build_inputs, native_build_inputs, makeRustPlatform }:
+let
+  rustBin = pkgs.rust-bin.stable.latest.default.override {
+    targets = [ "wasm32-unknown-unknown" ];
+  };
+
+  rustPlatform = makeRustPlatform {
+    cargo = rustBin;
+    rustc = rustBin;
+  };
+
+  common = {
+    version = "0.0.0";
+    src = ./.;
+    cargoLock.lockFile = ./Cargo.lock;
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+    buildInputs = build_inputs;
+    nativeBuildInputs = native_build_inputs;
+
+    LD_LIBRARY_PATH = lib.makeLibraryPath build_inputs;
+  };
+in {
+  lib =
+    rustPlatform.buildRustPackage (common // { pname = "bevy_web_file_drop"; });
+}
---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, flake-utils, nixpkgs, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustBin = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+
+        build_inputs = pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+          # Bevy
+          alsa-lib
+          libxkbcommon
+          udev
+          vulkan-loader
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ]);
+
+        native_build_inputs = with pkgs; [ cargo-auditable pkg-config ];
+
+        code = pkgs.callPackage ./. {
+          inherit pkgs system build_inputs native_build_inputs;
+        };
+      in rec {
+        packages = code // {
+          all = pkgs.symlinkJoin {
+            name = "all";
+            paths = with code; [ lib ];
+          };
+
+          default = packages.all;
+          override = packages.all;
+          overrideDerivation = packages.all;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [ cargo-watch rust-analyzer rustBin ] ++ build_inputs;
+          nativeBuildInputs = native_build_inputs;
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath build_inputs;
+        };
+      });
+}
