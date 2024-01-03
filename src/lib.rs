4ebbd649753939ea5b60e845ece0213428414314
Kayh <basedlainpilled@gmail.com>
2024-01-03 00:30:33 -0500

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Bevy plugin adding better support for drag and drop files in the web.
+//! Bevy has built in [drag and drop support](https://docs.rs/bevy/latest/bevy/prelude/enum.FileDragAndDrop.html), but when used in a web build they cause a panic and don't cancel the default browser behaviors.
+//! This plugin adds some custom JavaScript glue around the canvas to catch these events and relay them safely to Bevy.
+//!
+//! ## Usage
+//!
+//! After adding the plugin, you can use the same `FileDragAndDrop` events to read dropped files.
+//! The only difference is that the `path_buf` field of the `DroppedFile` event will be a blob URL instead of a local file path.
+//! This blob can be loaded as a Bevy asset using [bevy_blob_loader](https://docs.rs/bevy_blob_loader/latest/bevy_blob_loader/).
+
 use std::path::Path;
 
 use bevy::prelude::*;
