d767819cd425282332c0c5c0694cfc52731967b2
Kayh <basedlainpilled@gmail.com>
2024-07-09 16:09:53 -0400

    flake update

--- flake.nix
+++ flake.nix
@@ -2,38 +2,45 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs = {
-        flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
       };
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
-        rustBin = pkgs.rust-bin.stable.latest.default.override {
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           targets = [ "wasm32-unknown-unknown" ];
         };
 
-        build_inputs = pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
-          # Bevy
-          alsa-lib
-          libxkbcommon
-          udev
-          vulkan-loader
-          wayland
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-        ]);
+        build_inputs = pkgs.lib.optionals pkgs.stdenv.isLinux (
+          with pkgs;
+          [
+            alsa-lib
+            libxkbcommon
+            udev
+            vulkan-loader
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ]
+        );
 
         native_build_inputs = with pkgs; [
           cargo-auditable
@@ -43,13 +50,22 @@
         ];
 
         code = pkgs.callPackage ./. {
-          inherit pkgs system build_inputs native_build_inputs;
+          inherit
+            pkgs
+            system
+            build_inputs
+            native_build_inputs
+            ;
         };
-      in rec {
+      in
+      rec {
         packages = code // {
           all = pkgs.symlinkJoin {
             name = "all";
-            paths = with code; [ example lib ];
+            paths = with code; [
+              example
+              lib
+            ];
           };
 
           example = pkgs.symlinkJoin {
@@ -68,11 +84,19 @@
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ cargo-watch rust-analyzer rustBin ] ++ build_inputs;
+          buildInputs =
+            (with pkgs; [
+              cargo-watch
+              rust-analyzer
+            ])
+            ++ [ rustToolchain ]
+            ++ build_inputs;
           nativeBuildInputs = native_build_inputs;
 
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath build_inputs;
         };
-      });
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
